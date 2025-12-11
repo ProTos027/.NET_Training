8da6a9b061cb84b309ed6ec23e422458a0bcd250
Adityansh Srivastava <adisrivastava027@gmail.com>
2025-12-11 05:57:58 +0000

    setup c#( dotnet)

---
+++ tempApp/d1.cs
@@ -0,0 +1,8 @@
+// dotnet new console -o tempApp  ; used to create a new console app
+namespace A{
+    class D1{
+        static void Main(){
+            Console.WriteLine("Hello");
+        }
+    }
+}
